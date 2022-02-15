d7ce570ccb67f87c82cf2f29c701b71ac7706b14
neo-level <info@neo-level.com>
2022-02-15 20:43:15 +0100

    inital commit

---
+++ Source/EscapeTheMaze/EscapeTheMazeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeTheMazeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPETHEMAZE_API AEscapeTheMazeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
